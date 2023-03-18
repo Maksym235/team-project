5e4674ef6f8fceb135fc002625bb4e34e087a3f1
Roman Mokrushin <emptyspaceman1@gmail.com>
2023-03-18 15:57:16 +0200

    roma

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,38 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
 
-export const SearchForm = () => {
+export const SearchForm = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = e => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!query) {
+      alert('Select country!');
+      return;
+    }
+
+    onSubmit(query);
+    setQuery('');
+  };
+
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={handleSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select
+        aria-label="select"
+        name="region"
+        required
+        defaultValue="default"
+        onChange={handleChange}
+      >
+        <option disabled value="default">
           Select a region and press enter
         </option>
         <option value="africa">Africa</option>
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,37 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const query = searchParams.get('query');
+    if (!query) {
+      return;
+    }
+
+    async function getCountry() {
+      const data = await fetchByRegion(query);
+      setCountries([...data]);
+    }
+
+    getCountry();
+  }, [searchParams]);
+
+  const onSubmit = query => {
+    setSearchParams({ query });
+  };
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm onSubmit={onSubmit} />
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
