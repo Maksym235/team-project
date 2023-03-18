08d04275f5062c0ecae7799d2f24d6bd1188404b
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-18 15:32:33 +0100

    add

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,22 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  const [country, setCountry] = useState('');
+
+  
+  
+  useEffect(() => {
+  fetchCountry(countryId).then(resp => {setCountry(resp)});
+  }, [countryId])
+  
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <CountryInfo country={country } />
       </Container>
     </Section>
   );
